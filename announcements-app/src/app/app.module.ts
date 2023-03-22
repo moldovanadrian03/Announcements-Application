818ea04b785504ba489ff619fd96763a410ca3b8
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-23 01:27:54 +0200

    1. 2. Declare selectedCategory variable of type Category in AppComponent Generate categories component: ng generate c categories

--- announcements-app/src/app/app.module.ts
+++ announcements-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnnouncementComponent } from './announcement/announcement.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnnouncementComponent
+    AnnouncementComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
