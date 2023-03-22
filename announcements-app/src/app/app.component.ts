34cea09c7e420c1d123e107e12f1eb01a857bec7
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-23 00:27:37 +0200

    12. Declare an announcement type variable in app component

--- announcements-app/src/app/app.component.ts
+++ announcements-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Announcement } from './announcement';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'announcements-app';
+
+  announcement: Announcement;
 }
