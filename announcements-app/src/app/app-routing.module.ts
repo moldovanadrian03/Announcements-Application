b557ba096fb457f6b3fd1ce1d9ab20a9654173e7
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-23 04:16:12 +0200

    5. Set home component as default page in routes array

--- announcements-app/src/app/app-routing.module.ts
+++ announcements-app/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddAnnouncementFormComponent } from './add-announcement-form/add-announcement-form.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'add', component: AddAnnouncementFormComponent},
+  {path: '', component: HomeComponent, pathMatch: "full"},
   {path: '**', redirectTo: ''}
 ]
 
--- announcements-app/src/app/app.component.spec.ts
+++ announcements-app/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'announcements-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('announcements-app');
   });
 
   it('should render title', () => {
