8a672cab8b9ee0be9841d1b69c1fc5af786434da
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-23 03:11:01 +0200

    12. Create custom pipe for author to display “By Author” + name of author

--- announcements-app/src/app/app.module.ts
+++ announcements-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnnouncementComponent } from './announcement/announcement.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { MatButtonModule}  from '@angular/material/button';
+import { ByAuthorPipe } from './by-author.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     AnnouncementComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ByAuthorPipe
   ],
   imports: [
     BrowserModule,
