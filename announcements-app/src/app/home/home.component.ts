4ef64e078b7579c462e2e7bfc3e4d8940b10738d
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-03-29 18:43:54 +0300

    10. Remove the announcements list variable assignment of hardcoded values from HomeComponent.

--- announcements-app/src/app/home/home.component.ts
+++ announcements-app/src/app/home/home.component.ts
@@ -9,39 +9,8 @@ import { AnnouncementService } from '../services/announcement.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  announcement: Announcement[] =[
-    {
-      id: '1',
-      title: "title: adsa",
-      author: "author: asdsa",
-      message: "message: dasd",
-      category: {id: "1", name: "asadad"},
-      imageUrl: '03vision.info'
-    },
-    {
-      id: '2',
-      title:"title: adsadsad",
-      author: "author: asdsaadsadsa",
-      message:"message: dasddasdsa",
-      category: {id: "2", name: "asadadadsa"},
-      imageUrl: '03vision.info'
-    },
-    {
-      id: '3',
-      title:"Curs3",
-      author: "🌪️",
-      message:"message: dasddasdsa",
-      category: {id: "2", name: "asadadadsa"},
-      imageUrl: '03vision.info'
-    },
-    {
-      id: '4',
-      title:"Curs4",
-      author: "🌪️",
-      message:"message: dasddasdsa",
-      category: {id: "3", name: "adssa"},
-      imageUrl: '03vision.info'
-    }];    add: any;
+  announcement: Announcement[];
+    add: any;
 
     selectedCategory: Category;
     filteredAnnouncements: Announcement[] = this.announcement;
