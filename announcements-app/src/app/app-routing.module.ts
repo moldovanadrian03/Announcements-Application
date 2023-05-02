c9d53ca854c64f0bccd468661b420120f7add71a
Adrian <moldovan.adrian.alexandru03@gmail.com>
2023-05-02 13:52:15 +0300

    Routed the EditAnn component when an ann is clicked and added styling to the EditAnn component.

--- announcements-app/src/app/app-routing.module.ts
+++ announcements-app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AddAnnouncementFormComponent } from './add-announcement-form/add-announcement-form.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EditAnnouncementComponent } from './edit-announcement/edit-announcement.component';
 
 const routes: Routes = [
   {path: 'add', component: AddAnnouncementFormComponent},
   {path: 'add/:id', component: AddAnnouncementFormComponent},
+  {path: 'update/:id', component: EditAnnouncementComponent},
   {path: '', component: HomeComponent, pathMatch: "full"},
   {path: '**', redirectTo: ''}
 ]
